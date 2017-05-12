4e1cffe733484077d98a928192e074d855ef54f7
sriram570 <ssriram570@gmail.com>
2017-05-11 21:23:22 -0400

    added pc

--- static/js/main.js
+++ static/js/main.js
@@ -248,8 +248,8 @@ function color_map() {
 var pc_div = d3.select("#pc_div");
 
 var pc_margin = {top: 30, right: 10, bottom: 10, left: 10},
-    width = 960 - pc_margin.left - pc_margin.right,
-    height = 500 - pc_margin.top - pc_margin.bottom;
+    width = 1200 - pc_margin.left - pc_margin.right,
+    height = 450 - pc_margin.top - pc_margin.bottom;
 
 var x = d3.scale.ordinal().rangePoints([0, width], 1),
     y = {},
@@ -267,37 +267,62 @@ var pc_svg = pc_div.append("svg")
     .append("g")
     .attr("transform", "translate(" + pc_margin.left + "," + pc_margin.top + ")");
 
-d3.csv("./static/data/pc.csv", function(error, cars) {
-
+d3.csv("static/data/pc.csv", function(error, data) {
+  console.log();
+  console.log(data);
   // Extract the list of dimensions and create a scale for each.
-  x.domain(dimensions = d3.keys(cars[0]).filter(function(d) {
+  x.domain(dimensions = d3.keys(data[0]).filter(function(d) {
     return y[d] = d3.scale.linear()
-        .domain(d3.extent(cars, function(p) { return +p[d]; }))
+        .domain(d3.extent(data, function(p) { return +p[d]; }))
         .range([height, 0]);
   }));
 
   // Add grey background lines for context.
   background = pc_svg.append("g")
       .attr("class", "background")
-      .selectAll("path")
-      .data(cars)
-      .enter().append("path")
+    .selectAll("path")
+      .data(data)
+    .enter().append("path")
       .attr("d", path);
 
   // Add blue foreground lines for focus.
   foreground = pc_svg.append("g")
       .attr("class", "foreground")
-      .selectAll("path")
-      .data(cars)
-      .enter().append("path")
+    .selectAll("path")
+      .data(data)
+    .enter().append("path")
       .attr("d", path);
 
   // Add a group element for each dimension.
   var g = pc_svg.selectAll(".dimension")
       .data(dimensions)
-      .enter().append("g")
+    .enter().append("g")
       .attr("class", "dimension")
-      .attr("transform", function(d) { return "translate(" + x(d) + ")"; });
+      .attr("transform", function(d) { return "translate(" + x(d) + ")"; })
+      .call(d3.behavior.drag()
+        .origin(function(d) { return {x: x(d)}; })
+        .on("dragstart", function(d) {
+          dragging[d] = x(d);
+          background.attr("visibility", "hidden");
+        })
+        .on("drag", function(d) {
+          dragging[d] = Math.min(width, Math.max(0, d3.event.x));
+          foreground.attr("d", path);
+          dimensions.sort(function(a, b) { return position(a) - position(b); });
+          x.domain(dimensions);
+          g.attr("transform", function(d) { return "translate(" + position(d) + ")"; })
+        })
+        .on("dragend", function(d) {
+          delete dragging[d];
+          transition(d3.select(this)).attr("transform", "translate(" + x(d) + ")");
+          transition(foreground).attr("d", path);
+          background
+              .attr("d", path)
+            .transition()
+              .delay(500)
+              .duration(0)
+              .attr("visibility", null);
+        }));
 
   // Add an axis and title.
   g.append("g")
@@ -319,21 +344,24 @@ d3.csv("./static/data/pc.csv", function(error, cars) {
       .attr("width", 16);
 });
 
-
 function position(d) {
   var v = dragging[d];
   return v == null ? x(d) : v;
 }
+
 function transition(g) {
   return g.transition().duration(500);
 }
+
 // Returns the path for a given data point.
 function path(d) {
   return pc_line(dimensions.map(function(p) { return [position(p), y[p](d[p])]; }));
 }
+
 function brushstart() {
   d3.event.sourceEvent.stopPropagation();
 }
+
 // Handles a brush event, toggling the display of foreground lines.
 function brush() {
   var actives = dimensions.filter(function(p) { return !y[p].brush.empty(); }),
